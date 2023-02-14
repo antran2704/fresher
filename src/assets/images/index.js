8053518f09eddd99f223096cea5cecd7e75daef1
antran2704 <phamtrangiaan27@gmail.com>
2023-02-14 13:35:09 +0700

    up code

---
+++ src/assets/images/index.js
@@ -0,0 +1,40 @@
+const images = {
+    logo: require("./logo.png"),
+    arrow: require("./Arrow.png"),
+    apps: {
+        appStore: require("./apps/AppStore.png"),
+        chPlay: require("./apps/ChPlay.png"),
+        facebook: require("./apps/facebook.png"),
+        ni: require("./apps/ni.png"),
+        twitter: require("./apps/twitter.png"),
+    },
+    business: {
+        business1: require("./business/Business1.png"),
+        business2: require("./business/Business2.png"),
+        business3: require("./business/Business3.png"),
+    },
+    choose: {
+        fast: require("./Choose/Fast.png"),
+        profeshional: require("./Choose/Profeshional.png"),
+        quality: require("./Choose/Quality.png"),
+        restaurant: require("./Choose/Restaurant.png")
+    },
+    devices: {
+        phones: require("./devices/doubPhone.png"),
+        phone: require("./devices/iPhoneMini.png"),
+        laptop: require("./devices/Laptop.png")
+    },
+    feedbacks: {
+        feedback1: require("./feedback/feedback1.png"),
+        feedback2: require("./feedback/feedback2.png"),
+        feedback3: require("./feedback/feedback3.png"),
+    },
+    services: {
+        appliances: require("./services/appliances.png"),
+        cleaning: require("./services/Cleaning.png"),
+        electronic: require("./services/Electronic.png"),
+        houseHold: require("./services/Household.png")
+    }
+};
+
+export default images
